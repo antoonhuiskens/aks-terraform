fc2dd30af3df0f8bd32a2101039b1e1e015a86e5
Antoon Huiskens <antoon.huiskens@nginx.com>
2020-03-20 12:39:50 +0100

    refactoring of data.tf into sp.tf

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "azuread_application" "aks-tf-app" {
-  name = "ah-aks-tf"
-}
-
-data "azuread_service_principal" "aks-tf-sp" {
-  display_name = "ah-aks-tf"
-}
-
--- sp.tf
+++ sp.tf
@@ -1,3 +1,11 @@
+data "azuread_application" "aks-tf-app" {
+  name = "ah-aks-tf"
+}
+
+data "azuread_service_principal" "aks-tf-sp" {
+  display_name = "ah-aks-tf"
+}
+
 resource "azuread_application_password" "aks_sp_pwd" {
   application_object_id = data.azuread_application.aks-tf-app.id
   value                 = var.password

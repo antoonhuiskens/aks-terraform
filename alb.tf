62bb753e1550d4b123c0c6fbfd2649b4782445e4
Antoon Huiskens <antoon.huiskens@nginx.com>
2020-03-20 11:51:13 +0100

    Refactoring alb to refer to resources, not variables

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 resource "azurerm_lb" "aks-lb" {
   name                = var.lb-name
-  location            = var.location
+  location            = azurerm_resource_group.aks-rg.location
   resource_group_name = azurerm_resource_group.aks-rg.name
 }
 resource "azurerm_network_security_group" "aks-sg" {

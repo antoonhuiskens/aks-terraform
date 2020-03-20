734f1a27bdb977d529c2e746527a48b6c59e06f4
Antoon Huiskens <antoon.huiskens@nginx.com>
2020-03-20 13:00:52 +0100

    Adding comments to explain usage

--- sp.tf
+++ sp.tf
@@ -1,11 +1,13 @@
-data "azuread_application" "aks-tf-app" {
-  name = "ah-aks-tf"
-}
+# data.azuread_service_principal is needed both for aks and acr.
 
 data "azuread_service_principal" "aks-tf-sp" {
-  display_name = "ah-aks-tf"
+  display_name = var.ad_app
 }
 
+# Specify the name of an existing AD application:
+data "azuread_application" "aks-tf-app" {
+  name = var.ad_app
+}
 resource "azuread_application_password" "aks_sp_pwd" {
   application_object_id = data.azuread_application.aks-tf-app.id
   value                 = var.password
